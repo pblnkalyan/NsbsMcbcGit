545ce58bd9de0a849a18a2da516fe42c4cd0de39
Kalyan Pappu <pblnkalyan@gmail.com>
2020-10-13 20:53:09 +0530

    Customer Non Individual Screen Changes

---
+++ NsbSrilanka/src/CustomerNsbPackage/CustomerNonIndividualNsb.java
@@ -0,0 +1,13 @@
+package CustomerNsbPackage;
+
+import com.temenos.t24.api.hook.system.RecordLifecycle;
+
+/**
+ * TODO: Document me!
+ *
+ * @author kalpap
+ *
+ */
+public class CustomerNonIndividualNsb extends RecordLifecycle {
+
+}
